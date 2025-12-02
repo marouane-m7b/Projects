f3942faac1b39a9b03db8a801dece028f448fb78
marouane_m7b <marouane.ma7boub@gmail.com>
2025-12-02 10:32:25 +0100

    Session 1 Exercice 2

---
+++ Projet1/ex2.py
@@ -0,0 +1,8 @@
+"""
+exercice 2 : afficher les carres de nombre de 1 a 4
+format d'affichage : le carre de 1 est 1
+                     le carre de 2 est 4
+"""
+
+for i in range(1,5):
+    print(f"le carre de {i} est {i**2}")
