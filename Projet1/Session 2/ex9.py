d4bf3ce61ddd1fe97c932ed00ae7892743543661
marouane_m7b <marouane.ma7boub@gmail.com>
2025-12-02 12:05:50 +0100

    Edit Session 2 Exercice 9

--- Projet1/Session 2/ex9.py
+++ Projet1/Session 2/ex9.py
@@ -1,7 +1,11 @@
 num = float(input("Entrer Nombre Flottant :"))
 
+result = None
+
 for i in range(1, int(num)):
     if num > i ** 2 :
-        print(f"{i}")
+        result = i
     else:
         break
+
+print(result)
