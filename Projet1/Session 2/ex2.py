2c92c2ccc60be47a8e9f81560ab8b880b8ce92ec
marouane_m7b <marouane.ma7boub@gmail.com>
2025-12-02 10:58:33 +0100

    Session 2 Exercice 2

---
+++ Projet1/Session 2/ex2.py
@@ -0,0 +1,18 @@
+num1 = int(input("Nombre 1 :"))
+num2 = int(input("Nombre 2 :"))
+op = input("Operator :")
+result = 0
+
+if op == "+":
+    result = num1 + num2
+elif op == "-":
+    result = num1 - num2
+elif op == "x" or op == "*":
+    result = num1 * num2
+elif op == "/":
+    result = num1 / num2
+
+
+print(f"{num1} {op} {num2} = {result}")
+
+
