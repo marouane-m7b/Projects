0db37411651f052509ec0a7bde0eaafec8542e41
marouane_m7b <marouane.ma7boub@gmail.com>
2025-12-23 11:33:30 +0100

    Session 5 Cours Poo

---
+++ Projet1/Session 5/doc.py
@@ -0,0 +1,34 @@
+class Documant:
+    def __init__(self,titre:str,auteur:str,nbrPage:int=1)->None:
+        self.__titre = titre
+        self.auteur = auteur
+        self.nbrPage = nbrPage
+
+    def afficher(self):
+        print('Document: ')
+        print('Titre: ',self.__titre)
+        print('Auteur: ',self.auteur)
+        print('Nombre Page: ',self.nbrPage)
+
+    def __str__(self)->str:
+        return f"Titre: {self.__titre}, Auteur: {self.auteur} Nombre Page: {self.nbrPage}"
+    
+    # def getTitre(self):
+    #     return(self.__titre)
+    
+    # def setTitre(self,titre):
+    #     self.__titre=titre
+
+    @property
+    def titre(self):
+        return self.__titre
+    
+    @titre.setter
+    def titre(self,value):
+        self.__titre = value
+
+
+# doc1 = Documant()
+# print(doc1)
+# doc2 = Documant("Fille de papier","Musso",367)
+# doc2.afficher()
---
+++ Projet1/Session 5/test.py
@@ -0,0 +1,10 @@
+from doc import Documant
+
+doc = Documant('One Piece','Eichiro Oda',49039)
+
+print(doc)
+
+doc.afficher()
+print(doc.titre)
+doc.titre = 'The One Piece'
+print(doc.titre)
