896d6fcae0ec1f94101b219baae2168a34a76e3d
marouane_m7b <marouane.ma7boub@gmail.com>
2025-12-23 10:31:06 +0100

    tp

--- Projet1/Session 4/tp2ex1.py
+++ Projet1/Session 4/tp2ex1.py
@@ -1,8 +1,9 @@
 List1= [3,4,5,3,4,5,1]
 
 for i in range(0,len(List1)):
-    for j in range(i,len(List1)-1):
-        if i == j:
-            List1.pop(i)
+    for j in range(i,len(List1)):
+        if List1[i] == List1[j] and i != j:
+            List1.remove(List1[j])
+            break
 
 print(List1)
